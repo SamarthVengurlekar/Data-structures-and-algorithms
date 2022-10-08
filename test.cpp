c3d935655b0833387e8a9c1da44e3ce1c2307c54
SamarthVengurlekar <112164785+SamarthVengurlekar@users.noreply.github.com>
2022-10-08 18:00:59 +0530

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int A[5];
+    A[0]=12;
+    A[1]=15;
+    A[2]=25;
+     cout<<sizeof(A)<<endl;
+     cout<<A[1]<<endl;
+    printf("%d\n",A[2]);
+   
+     return 0;
+}
