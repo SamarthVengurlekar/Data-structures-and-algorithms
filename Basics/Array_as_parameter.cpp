c91dba6c5e6742ad66e765d71620fa0c9b48c14d
SamarthVengurlekar_GitHub <Samarthvenga@gmail.com>
2022-10-12 01:00:59 +0530

    .

--- Basics/Array_as_parameter.cpp
+++ Basics/Array_as_parameter.cpp
@@ -1,7 +1,27 @@
 #include<iostream>
 using namespace std;
+
+void fun(int A[], int n) //call by value
+{
+    int i;
+    for( i=0;i<n;i++)
+        printf("%d",A[i]);
+
+        cout<<sizeof(A)/sizeof(int)<<endl;
+}
+
 int main()
 {
-    cout<<"Hello";
+    int A[5]={2,4,6,8,10};
+    fun(A,5);
+        cout<<sizeof(A)/sizeof(int)<<endl;
+
  return 0;
 }
+
+
+
+
+
+//arrays can be passed only using address
+//function can modify array as call by adress
