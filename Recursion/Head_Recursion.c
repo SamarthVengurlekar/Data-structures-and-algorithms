3cc456bb2a7b481774f20e6cc51ece68e9ae71bf
SamarthVengurlekar_GitHub <Samarthvenga@gmail.com>
2022-10-15 12:43:56 +0530

    Update Head_Recursion.c

--- Recursion/Head_Recursion.c
+++ Recursion/Head_Recursion.c
@@ -1,4 +1,16 @@
 //HEAD RECURSION:- The function doesen't have to perform any operation at the time of calling it has to eveythig at the time of returning 
+//Head recursion csnnot be easily converted in loop as it is but can be convertable
+
+// void fun (int n){
+//     int i=1;
+//     while  (i<=n)
+//     {
+//         printf("%d",n);
+//         i++;
+//     }
+
+// }
+
 
 #include <stdio.h>
 
