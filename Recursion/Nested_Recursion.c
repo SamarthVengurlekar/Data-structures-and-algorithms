a019e3e555a589fdff94de70a825b4208b8f21ac
SamarthVengurlekar_GitHub <Samarthvenga@gmail.com>
2022-10-15 15:33:06 +0530

    .

---
+++ Recursion/Nested_Recursion.c
@@ -0,0 +1,17 @@
+//NESTED RECURSION:-In this a recursive function will pass parameter as a recursive call ["Recursion inside Recursion"]
+
+#include <stdio.h>
+
+int fun(int n)
+{
+    if(n>100)
+        return n-10;
+    return fun(fun(n+11));
+}
+int main()
+{
+    int r;
+    r=fun(30);
+    printf("%d",r);
+    return 0;
+}
